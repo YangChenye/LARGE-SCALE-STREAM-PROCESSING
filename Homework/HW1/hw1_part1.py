68dc93e1cc6b1c57972bf33be485161608ebe3f2
Chenye Yang <cy2540@columbia.edu>
2020-02-12 12:24:58 -0500

    HW1 Done

--- Homework/HW1/hw1_part1.py
+++ Homework/HW1/hw1_part1.py
@@ -3,7 +3,15 @@
 
 import pyspark
 
-sc = pyspark.SparkContext()
-logfile = sc.textFile('epa-http.txt')
-print(logfile.count())
-print(logfile.first())
+sc = pyspark.SparkContext() # creat a spark context object
+log_lines = sc.textFile('epa-http.txt') # read file line by line to creat RDDs
+# transform RDDs into pair RDDs ('ip',bytes). Key: IP address. Value: Bytes served.
+# use map() because one input RDD corresponding to one output RDD
+# use ' ' to split the string and select first item as key, last item as value
+# last value may be '-', which can not be transferred to int and should be counted as 0
+log_ip_bytes_pairs = log_lines.map(lambda x: (x.split(' ')[0], int(x.split(' ')[-1]) if x.split(' ')[-1] != '-' else 0))
+# count the total bytes served to same IP addresses, according to keys in ('ip',bytes) pairs
+log_ip_total_bytes = log_ip_bytes_pairs.reduceByKey(lambda x, y: x + y)
+
+print(log_ip_total_bytes.count()) # count the number of different IPs
+print(log_ip_total_bytes.first()) # check the total bytes served to first IP
