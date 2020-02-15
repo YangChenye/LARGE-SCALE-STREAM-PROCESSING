7a528b2c6ff4d74dcbdb02542426b43ce8c3f69a
Chenye Yang <cy2540@columbia.edu>
2020-02-15 13:37:17 -0500

    HW1 Part3 Changed

--- Homework/HW1/hw1_part3.py
+++ Homework/HW1/hw1_part3.py
@@ -7,6 +7,7 @@ from pyspark.sql.types import *
 from datetime import datetime
 from pyspark.sql.functions import *
 import re
+import csv
 
 # Make a regular expression for validating an Ip-address
 regex = r'(([01]{0,1}\d{0,1}\d|2[0-4]\d|25[0-5])\.){3}([01]{0,1}\d{0,1}\d|2[0-4]\d|25[0-5])'
@@ -48,8 +49,11 @@ log_dataframe = spark.createDataFrame(log_pairs, schema)
 # with window size = 60min slide = 60min, we group the log in one hour by "datetime"
 # with groupBy, we groups the DataFrame using the specified columns, so we can run aggregation on them.
 # with agg, we aggregate the items in window and "ip", the result should be the sum of "bytes"
-# with orderBy, we get a new DataFrame sorted by the specified column(s) by ascending or descending
+# with orderBy, we get new DataFrame sorted by the specified column(s) by timestamp ascending and total bytes descending
 # with show, we print 20 sorted items without truncating strings longer than 20 chars
+# CONTENT IN log_windowed:
+# columns in log_windowed are ['window', 'ip', 'sum(bytes)']
+# data types are [('window', 'struct<start:timestamp,end:timestamp>'), ('ip', 'string'), ('sum(bytes)', 'bigint')]
 log_windowed = log_dataframe \
     .withWatermark('datetime', '10 minutes') \
     .groupBy(window(log_dataframe.datetime, '60 minutes', '60 minutes'), log_dataframe.ip) \
@@ -57,9 +61,21 @@ log_windowed = log_dataframe \
     .orderBy(['window.start', 'sum(bytes)'], ascending=[True, False])
 log_windowed.show(20, truncate=False)
 
-start_time = datetime.strptime('20-01-{}{}'.format('30', '00:00:00'), '%y-%m-%d%H:%M:%S')
-end_time = datetime.strptime('20-01-{}{}'.format('30', '00:59:59'), '%y-%m-%d%H:%M:%S')
-# log_csv = log_windowed.select('ip', 'sum(bytes)').where('window >= start_time and window <= end_time')
-log_csv = log_windowed.filter("window >= start_time and window <= end_time")
+# window.start and window.end are timestamps and we can use the built in method to get year(), month(), day()
+# use filter() and SQL like commands to select the specific data in one day between some hours
+log_specific_time_interval = log_windowed \
+    .filter('year(window.start) == 2020') \
+    .filter('month(window.start) == 01') \
+    .filter('day(window.start) == 30') \
+    .filter('Hour(window.start) == 00 AND Hour(window.end) == 01')
 
+log_specific_time_interval.show(20, truncate=False)
 
+# drop the 'window' column because it is not in output csv file
+# sort by ip in string ascending order so 100.1.2.3 comes before 99.2.3.5.
+log_csv = log_specific_time_interval.drop('window').sort('ip', ascending=True)
+
+# save RDD as CSV
+f = open('hw1_part3.csv', 'w', newline='')
+f_csv = csv.writer(f)
+f_csv.writerows(log_csv.collect())
