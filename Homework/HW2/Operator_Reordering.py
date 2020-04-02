3247d1114408e72c8a91f308c0aae72c36dcb5c5
Chenye Yang <cy2540@columbia.edu>
2020-04-02 12:48:20 -0400

    Update Operator_Reordering.py

--- Homework/HW2/Operator_Reordering.py
+++ Homework/HW2/Operator_Reordering.py
@@ -7,6 +7,7 @@ import csv
 import random
 from time import process_time
 import matplotlib.pyplot as plt
+import numpy as np
 
 # First operator is A, second operator is B
 def A_B(log_lines, selectRateA, selectRateB, allowOutput = False):
@@ -41,40 +42,48 @@ if __name__ == '__main__':
     sc = pyspark.SparkContext.getOrCreate(conf=conf)  # creat a spark context object
     log_lines = sc.textFile('epa-http.txt')  # read file line by line to create RDDs
 
-    throughput_A_B = []
-    throughput_B_A = []
+    throughput_A_Bs = []
+    throughput_B_As = []
 
-    # Not reordered
-    for selectRateB in selectRateBs:
-        start = process_time()
-        A_B(log_lines, selectRateA, selectRateB, allowOutput=False)
-        end = process_time()
-        A_B_time = end - start
-        print(A_B_time)
-        throughput_A_B.append(A_B_time)
+    for i in range(100):
+        throughput_A_B = []
+        throughput_B_A = []
 
-    # Reordered
-    for selectRateB in selectRateBs:
-        start = process_time()
-        B_A(log_lines, selectRateA, selectRateB, allowOutput=False)
-        end = process_time()
-        B_A_time = end - start
-        print(B_A_time)
-        throughput_B_A.append(B_A_time)
+        # Not reordered
+        for selectRateB in selectRateBs:
+            start = process_time()
+            A_B(log_lines, selectRateA, selectRateB, allowOutput=False)
+            end = process_time()
+            A_B_time = end - start
+            # print(A_B_time)
+            throughput_A_B.append(A_B_time)
 
+        # Reordered
+        for selectRateB in selectRateBs:
+            start = process_time()
+            B_A(log_lines, selectRateA, selectRateB, allowOutput=False)
+            end = process_time()
+            B_A_time = end - start
+            # print(B_A_time)
+            throughput_B_A.append(B_A_time)
 
-    throughput_A_B = list(map(lambda x: 1 / x, throughput_A_B))
-    throughput_B_A = list(map(lambda x: 1 / x, throughput_B_A))
+        # throughput_A_B = list(map(lambda x: 4448989 / x, throughput_A_B)) # the size of epa-http.txt is 4448989 Byte
+        # throughput_B_A = list(map(lambda x: 4448989 / x, throughput_B_A))
 
+        throughput_A_Bs.append(throughput_A_B)
+        throughput_B_As.append(throughput_B_A)
 
-    maxInAB = max(throughput_A_B)
+    throughput_A_B_mean = np.mean(np.array(throughput_A_Bs), axis=0).tolist()
+    throughput_B_A_mean = np.mean(np.array(throughput_B_As), axis=0).tolist()
+
+    maxInAB = max(throughput_A_B_mean)
     minInAB = 0
-    throughput_A_B = list(map(lambda x: (x - minInAB) / (maxInAB - minInAB), throughput_A_B))
-    throughput_B_A = list(map(lambda x: (x - minInAB) / (maxInAB - minInAB), throughput_B_A))
+    throughput_A_B_mean = list(map(lambda x: (x - minInAB) / (maxInAB - minInAB), throughput_A_B_mean))
+    throughput_B_A_mean = list(map(lambda x: (x - minInAB) / (maxInAB - minInAB), throughput_B_A_mean))
 
 
-    plt.plot(selectRateBs, throughput_A_B, 'r-', label='Not reordered')
-    plt.plot(selectRateBs, throughput_B_A, 'b--', label='Reordered')
+    plt.plot(selectRateBs, throughput_A_B_mean, 'r-', label='Not reordered')
+    plt.plot(selectRateBs, throughput_B_A_mean, 'b--', label='Reordered')
     plt.xlabel('Selectivity of B')
     plt.ylabel('Throughput')
     plt.legend()
