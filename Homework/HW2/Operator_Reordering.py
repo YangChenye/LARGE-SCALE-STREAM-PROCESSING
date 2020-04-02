e779cdf9f821c3901f55702d5737fd222f6bd310
Chenye Yang <cy2540@columbia.edu>
2020-04-01 23:14:00 -0400

    Update Operator_Reordering.py

--- Homework/HW2/Operator_Reordering.py
+++ Homework/HW2/Operator_Reordering.py
@@ -14,11 +14,13 @@ def A_B(selectRateA, selectRateB, iteration):
         conf = pyspark.SparkConf().setAppName('hw1_part1_ChenyeYang').setMaster('local[*]')
         sc = pyspark.SparkContext(conf=conf) # creat a spark context object
         log_lines = sc.textFile('../epa-http.txt') # read file line by line to create RDDs
-
+        print('Lines input: {}'.format(log_lines.count()))
         # stream pass through operator A
         log_lines_A = log_lines.filter(lambda x: random.random() > selectRateA)
+        print('Lines after operator A: {}'.format(log_lines_A.count()))
         # stream pass through operator B
         log_lines_B = log_lines_A.filter(lambda x: random.random() > selectRateB)
+        print('Lines after operator B: {}'.format(log_lines_B.count()))
 
 # First operator is B, second operator is A
 def B_A(selectRateA, selectRateB, iteration):
@@ -27,11 +29,13 @@ def B_A(selectRateA, selectRateB, iteration):
         conf = pyspark.SparkConf().setAppName('hw1_part1_ChenyeYang').setMaster('local[*]')
         sc = pyspark.SparkContext(conf=conf)  # creat a spark context object
         log_lines = sc.textFile('../epa-http.txt')  # read file line by line to create RDDs
-
+        print('Lines input: {}'.format(log_lines.count()))
         # stream pass through operator B
         log_lines_B = log_lines.filter(lambda x: random.random() > selectRateB)
+        print('Lines after operator B: {}'.format(log_lines_B.count()))
         # stream pass through operator A
         log_lines_A = log_lines_B.filter(lambda x: random.random() > selectRateA)
+        print('Lines after operator A: {}'.format(log_lines_A.count()))
 
 
 
