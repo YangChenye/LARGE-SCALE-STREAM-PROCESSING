7744ab022c4672e4bc2dd7b29b892ebfcf01a786
Chenye Yang <cy2540@columbia.edu>
2020-04-01 23:08:23 -0400

    Update Operator_Reordering.py

--- Homework/HW2/Operator_Reordering.py
+++ Homework/HW2/Operator_Reordering.py
@@ -20,9 +20,18 @@ def A_B(selectRateA, selectRateB, iteration):
         # stream pass through operator B
         log_lines_B = log_lines_A.filter(lambda x: random.random() > selectRateB)
 
+# First operator is B, second operator is A
+def B_A(selectRateA, selectRateB, iteration):
+    throughput = []
+    for i in range(iteration):
+        conf = pyspark.SparkConf().setAppName('hw1_part1_ChenyeYang').setMaster('local[*]')
+        sc = pyspark.SparkContext(conf=conf)  # creat a spark context object
+        log_lines = sc.textFile('../epa-http.txt')  # read file line by line to create RDDs
 
-
-
+        # stream pass through operator B
+        log_lines_B = log_lines.filter(lambda x: random.random() > selectRateB)
+        # stream pass through operator A
+        log_lines_A = log_lines_B.filter(lambda x: random.random() > selectRateA)
 
 
 
